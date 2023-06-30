fcd529b293d0814d737325367954a980a6ab1d6f
cstandy <cst.andy@gmail.com>
2023-06-30 09:37:08 -0400

    Feat: Plot CFO

---
+++ plot_cfo.py
@@ -0,0 +1,96 @@
+################################################################################
+# Plot CFO distribution as histogram. Output seperate figures for uplink and 
+# downlink. Overall output two plots.
+# author: Chung-Hsuan Tung
+################################################################################
+
+import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
+from scipy.io import loadmat
+import numpy as np
+
+################################################################################
+# Functions for plot attributes
+################################################################################
+
+def kilos(x, pos):
+    'The two args are the value and tick position'
+    return '%1.1fk' % (x*1e-3)
+
+################################################################################
+# Font settings: tick size, linewidth, marker size
+################################################################################
+
+# Font sizes
+
+# plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+# plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=24)     # fontsize of the x and y labels
+plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
+plt.rc('legend', fontsize=16)    # legend fontsize
+# plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+# plt.rcParams.update({'font.size': 16})
+
+FIG_SIZE_W = 6
+FIG_SIZE_H = 4.5
+
+edgecolor='black'
+
+plt.figure(figsize=(FIG_SIZE_W, FIG_SIZE_H))
+
+################################################################################
+# I/O format
+################################################################################
+
+#
+# Input
+#
+
+input_filepath = './data/'
+input_filename = 'CFO_TX2_RX3_trial1.mat'
+# input_filename = 'CFO_TX2_RX3_trial2.mat'
+# input_filename = 'CFO_TX3_RX2_trial1.mat'
+# input_filename = 'CFO_TX3_RX2_trial2.mat'
+input_file = input_filepath + input_filename
+
+data = loadmat(input_file)
+cfo = data['cfoList'][0] # data is double-encapsulated
+
+#
+# Output
+#
+
+# output_format = 'png'
+# output_format = 'svg'
+output_format = 'pdf'
+
+output_filepath = './fig/'
+output_fileprefix = output_filepath
+
+################################################################################
+# Plot CFO
+################################################################################
+
+fig, ax = plt.subplots()
+formatter = FuncFormatter(kilos)
+ax.xaxis.set_major_formatter(formatter)
+
+binwidth = 100
+neg = 0
+l_bound = -100000 if neg == 1 else 90000
+h_bound = -90000 if neg == 1 else 100000
+plt.hist(cfo, edgecolor=edgecolor, bins=range(l_bound, h_bound, binwidth))
+
+# plt.xlim(10, 35)
+plt.ylim(0, 15)
+plt.yticks(np.arange(0, 16, 5))
+plt.xticks(np.arange(l_bound, h_bound+1, step=2500))
+plt.title(input_filename)
+plt.xlabel('Frequency')
+plt.ylabel('Distribution')
+plt.grid()
+plt.savefig(output_fileprefix + 'CFO.' + output_format,
+            format=output_format,
+            bbox_inches='tight')
+plt.clf()
