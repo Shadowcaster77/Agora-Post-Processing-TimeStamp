15900943dcab797f647d409d35eeb2bf58a6d4fe
cstandy <cst.andy@gmail.com>
2023-06-30 03:35:21 -0400

    Feat: Plot EVM based on modulation (16/64QAM)
    Preprocess the data and plot the evm for 16/64QAM as two seperate figures.
    
    - SISO and MIMO cases are included.
    - 2/3 code rate (mcs-3/-6) is selected.

---
+++ plot_evm.py
@@ -0,0 +1,242 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+from brokenaxes import brokenaxes
+from scipy.io import loadmat
+import numpy as np
+
+################################################################################
+# Functions for plot attributes
+################################################################################
+
+def get_marker(pol):
+    # marker = ['o', '^', 'x']
+    marker = 'x'
+    if pol == 'H':
+        marker = 'o'
+    if pol == 'V':
+        marker = '^'
+
+    # if mod == '16QAM':
+    #     marker = 'o'
+    # if mod == '64QAM':
+    #     marker = '^'
+    return marker
+
+def get_color(pol):
+    ''' color = 'tab:blue'
+              'tab:orange'
+              'tab:green'
+              'tab:red'
+              'tab:purple'
+              'tab:brown'
+              'tab:pink'
+              'tab:gray'
+              'tab:olive'
+              'tab:cyan'''
+    color = 'tab:green'
+    if pol == 'H':
+        color = 'tab:blue'
+    if pol == 'V':
+        color = 'tab:orange'
+    return color
+
+def get_linestyle(chan):
+    ls = '--'
+    if chan == 'MIMO':
+        ls = '-'
+    if chan == 'SISO':
+        ls = ':'
+    return ls
+
+def get_evm_req(mod):
+    res = 0
+    if mod == '16QAM':
+        res = 12.5
+    if mod == '64QAM':
+        res = 8
+    return res
+
+################################################################################
+# Font settings: tick size, linewidth, marker size
+################################################################################
+
+# Font sizes
+
+# plt.rc('font', size=SMALL_SIZE)          # controls default text sizes
+# plt.rc('axes', titlesize=SMALL_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize=24)     # fontsize of the x and y labels
+plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
+plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
+plt.rc('legend', fontsize=20)    # legend fontsize
+# plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+# plt.rcParams.update({'font.size': 16})
+
+FIG_SIZE_W = 6
+FIG_SIZE_H = 4.5
+
+markeredgecolor='black'
+markeredgewidth=2
+
+plt.figure(figsize=(FIG_SIZE_W, FIG_SIZE_H))
+plt.rc('lines', linewidth=5)
+plt.rc('lines', markersize=15)
+plt.rc('lines', markeredgecolor=markeredgecolor)
+plt.rc('lines', markeredgewidth=markeredgewidth)
+
+################################################################################
+# Param settings
+################################################################################
+
+chan_type = ['SISO', 'MIMO'] # channel type
+pol = ['H', 'V'] # polarization
+mod_type = ['16QAM', '64QAM'] # modulation type
+# code_rate = ['333', '500', '666'] # n/1000
+# snr_ideal = ['5', '10', '13', '15', '20', '25', '30']
+
+###
+# MCS | modulation | code_rate 
+#  1      16qam         1/3
+#  2      16qam         1/2
+#  3      16qam         2/3
+#  4      64qam         1/3
+#  5      64qam         1/2
+#  6      64qam         2/3
+mcs = [3, 6] # Target MCS to plot
+
+################################################################################
+# I/O filename/format
+################################################################################
+
+#
+# Input
+#
+
+input_filepath = './data/'
+input_fileprefix = input_filepath + 'result_'
+
+#
+# Output
+#
+
+# output_format = 'png'
+# output_format = 'svg'
+output_format = 'pdf'
+
+output_filepath = './fig/'
+
+################################################################################
+# Read data & pre-processing
+################################################################################
+
+#
+# Read data
+#
+
+# Input files:
+#     result_MIMO_H.mat
+#     result_MIMO_V.mat
+#     result_SISO_H.mat
+#     result_SISO_V.mat
+#     dimension - [chan_type][polarization]
+
+# In each file, format:
+#    dict:  snrList, evmList, berList
+# In each list:
+#    dimension - [mcs][snr_ideal], snr_ideal is the x axis
+
+# read data as dict: [chan_type][pol]
+data_in = {}
+for chan in chan_type:
+    data_in[chan] = {}
+    for p in pol:
+        data_temp = loadmat('{0}result_{1}_{2}.mat'.format(input_filepath, chan, p))
+        data_in[chan][p] = {}
+        data_in[chan][p]['snr'] = data_temp['snrList']
+        data_in[chan][p]['evm'] = data_temp['evmList']
+        data_in[chan][p]['ber'] = data_temp['berList']
+
+#
+# Pre-processing
+#
+
+# retreive mcs dimension
+snr_temp = data_in[chan_type[0]][pol[0]]['snr']
+num_mcs = len(snr_temp)
+# num_snr = len(snr_temp[0])
+
+# re-format data into diemension [mod_type][chan_type][pol]
+# 1. init data dict
+data = {}
+for mod in mod_type:
+    data[mod] = {}
+    for chan in chan_type:
+        data[mod][chan] = {}
+        for p in pol:
+            data[mod][chan][p] = {}
+
+# 2. read based on mcs
+for chan in chan_type:
+    for p in pol:
+        for mcs_idx in range(num_mcs):
+            # accumulate evm
+            # mcs 1-3: 16QAM, mcs 4-6: 64QAM
+            if mcs_idx+1 in mcs:
+                mod = mod_type[0] if mcs_idx < 3 else mod_type[1]
+                data[mod][chan][p]['snr'] = data_in[chan][p]['snr'][mcs_idx]
+                data[mod][chan][p]['evm'] = data_in[chan][p]['evm'][mcs_idx]
+
+
+################################################################################
+# Plot EVM
+################################################################################
+
+for mod in mod_type:
+    # Plot based on modulation scheme
+
+    # First loop can plot everything escept for the marker edge
+    for chan in chan_type:
+        for p in pol:
+            # cr = code_rate[mcs_idx % 3]
+            marker = get_marker(p) # marker: o, x, s, ^
+            color = get_color(p) # color: default scheme
+            line = get_linestyle(chan) # line: -, --, :
+            label = chan + '-' + p
+
+            snr = data[mod][chan][p]['snr']
+            evm = data[mod][chan][p]['evm']
+
+            plt.plot(snr, evm, linestyle=line, marker=marker, label=label, color=color)
+
+    # Second loop brings the marker to front
+    for chan in chan_type:
+        for p in pol:
+            # cr = code_rate[mcs_idx % 3]
+            marker = get_marker(p) # marker: o, x, s, ^
+            color = get_color(p) # color: default scheme
+            line = get_linestyle(chan) # line: -, --, :
+            label = chan + '-' + p
+
+            snr = data[mod][chan][p]['snr']
+            evm = data[mod][chan][p]['evm']
+
+            plt.scatter(snr, evm, linestyle='-', marker=marker, color=color,
+                        edgecolors=markeredgecolor,
+                        linewidth=markeredgewidth,
+                        zorder=10)
+    
+    # Standard EVM
+    evm_req = get_evm_req(mod)
+    plt.axhline(y = evm_req, color = 'r', linestyle = '--')
+
+    plt.xlim(10, 35)
+    plt.ylim(0, 50)
+    plt.xticks(np.arange(10, 36, step=5))
+    plt.xlabel('SNR (dB)')
+    plt.ylabel('EVM (%)')
+    # plt.title('EVM vs SNR')
+    plt.legend()
+    plt.grid()
+    plt.savefig(output_filepath + 'EVM_' + mod + '.' + output_format,
+                format=output_format,
+                bbox_inches='tight')
+    plt.clf()
