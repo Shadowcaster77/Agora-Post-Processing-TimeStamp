023b8a42b863d08979254d97916635308728e290
cstandy <cst.andy@gmail.com>
2023-06-30 17:05:29 -0400

    Feat: Plot MIMO constellation

--- plot_conste.py
+++ plot_conste.py
@@ -12,6 +12,18 @@ import numpy as np
 # Functions for plot attributes
 ################################################################################
 
+################################################################################
+# Param settings
+################################################################################
+
+chan_type = ['SISO', 'MIMO'] # channel type
+pol = ['H', 'V'] # polarization
+mod_type = ['16QAM', '64QAM'] # modulation type
+# code_rate = ['333', '500', '666'] # n/1000
+# snr_ideal = ['5', '10', '13', '15', '20', '25', '30']
+
+chan = chan_type[1]
+mod = mod_type[0]
 
 ################################################################################
 # Font settings: tick size, linewidth, marker size
@@ -25,10 +37,10 @@ plt.rc('axes', labelsize=24)     # fontsize of the x and y labels
 plt.rc('xtick', labelsize=20)    # fontsize of the tick labels
 plt.rc('ytick', labelsize=20)    # fontsize of the tick labels
 plt.rc('legend', fontsize=20)    # legend fontsize
-# plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
+# plt.rc('figure', titlesize=20)  # fontsize of the figure title
 # plt.rcParams.update({'font.size': 16})
 
-FIG_SIZE_W = 6
+FIG_SIZE_W = 5.8
 FIG_SIZE_H = 6
 
 markeredgecolor='tab:blue'
@@ -47,12 +59,13 @@ plt.rc('lines', markersize=10)
 #
 
 input_filepath = './data/'
-input_file = input_filepath + 'result_conste_11.mat'
+input_file = input_filepath + 'result_conste_' + chan + '_' + mod + '.mat'
+# input_file = input_filepath + 'result_conste_11.mat'
 
 data = loadmat(input_file)
 evm = data['evm']
-real = data['real']
-imag = data['imag']
+real = data['real'][0]
+imag = data['imag'][0]
 
 #
 # Output
@@ -63,7 +76,7 @@ imag = data['imag']
 output_format = 'pdf'
 
 output_filepath = './fig/'
-output_fileprefix = output_filepath
+output_fileprefix = output_filepath + 'conste_' + chan + '_' + mod
 
 ################################################################################
 # Print EVM info
@@ -76,9 +89,6 @@ print('EVM = {:.2%}'.format(evm))
 ################################################################################
 # Create Reference Grid
 ################################################################################
-
-mod_type = ['16QAM', '64QAM']
-mod = mod_type[0]
 
 ref_symbols = {
     '16QAM': {
@@ -102,17 +112,30 @@ for i in ref_symbols[mod]['I']:
 
 # Plot the reference
 for r in ref:
-    plt.scatter(*r, facecolors='red', zorder=15)
+    plt.scatter(*r, facecolors='black', zorder=15)
 
 # Plot the constellation
-plt.scatter(real, imag,
-            facecolors='none',
-            edgecolors=markeredgecolor,
-            linewidth=markeredgewidth,
-            zorder=10)
-            # marker=marker,
-            # label=label,
-            # color=color)
+if chan == 'SISO':
+    plt.scatter(real, imag,
+                facecolors='none',
+                edgecolors=markeredgecolor,
+                linewidth=markeredgewidth,
+                zorder=10)
+elif chan == 'MIMO':
+    mid = int(len(real)/2) + 1
+    plt.scatter(real[:mid], imag[:mid],
+                facecolors='none',
+                edgecolors='tab:blue',
+                linewidth=markeredgewidth,
+                zorder=10)
+    
+    plt.scatter(real[mid:], imag[mid:],
+                facecolors='none',
+                edgecolors='tab:orange',
+                linewidth=markeredgewidth,
+                zorder=10)
+else:
+    print('Must specify channel type! Available options: \'SISO\', \'MIMO\'')
 
 l_bound = -1.5
 h_bound = 1.5
@@ -122,11 +145,11 @@ plt.xlim(l_bound, h_bound)
 plt.ylim(l_bound, h_bound)
 plt.xticks(np.arange(l_bound, h_bound + 0.01, step))
 plt.yticks(np.arange(l_bound, h_bound + 0.01, step))
-# # plt.title(input_filename)
+plt.title('EVM = {:.2%}'.format(evm), fontsize=28)
 plt.xlabel('I')
 plt.ylabel('Q')
 plt.grid()
-plt.savefig(output_fileprefix + 'conste.' + output_format,
+plt.savefig(output_fileprefix + '.' + output_format,
             format=output_format,
             bbox_inches='tight')
 plt.clf()
