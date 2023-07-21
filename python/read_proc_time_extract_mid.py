d52a1e4779f21d93be81aae8e1f7f92e0813125a
cstandy <cst.andy@gmail.com>
2023-07-20 23:15:54 -0400

    Chores: Rewrite proc time log reading script
    1. Structure it so that it can be call as func and run directly
    2. Name var in a consistent way
    3. Update print format

--- python/read_proc_time_extract_mid.py
+++ python/read_proc_time_extract_mid.py
@@ -11,61 +11,61 @@ Description: Read from Agora's stdout screen log and plot the processing time
 import regex as re
 from optparse import OptionParser
 
-thres = 1500
+THRES = 1500
 
-parser = OptionParser()
-parser.add_option("--file", type="string", dest="file_name", help="file name as input", default="")
-(options, args) = parser.parse_args()
-file_str = options.file_name
+def proc_time(filename, thres):
 
+    f = open(filename, 'r')
+    lines = f.read()
 
-f = open(file_str, 'r')
-lines = f.read()
+    fft_time = re.findall('(?<=FFT \(\d*\ tasks\):\s+)[\d\.]+', lines)
+    fft_time_list = list(map(float, fft_time))
 
+    csi_time = re.findall('(?<=CSI \(\d*\ tasks\):\s+)[\d\.]+', lines)
+    csi_time_list = list(map(float, csi_time))
 
-fft = re.findall('(?<=FFT \(\d*\ tasks\):\s+)[\d\.]+',lines);
-fft_time = list(map(float, fft))
+    bw_time = re.findall('(?<=Beamweights \(\d*\ tasks\):\s+)[\d\.]+', lines)
+    bw_time_list = list(map(float, bw_time))
 
+    demul_time = re.findall('(?<=Demul \(\d*\ tasks\):\s+)[\d\.]+', lines)
+    demul_time_list = list(map(float, demul_time))
 
-csi = re.findall('(?<=CSI \(\d*\ tasks\):\s+)[\d\.]+',lines);
-csi_time = list(map(float, csi))
+    decode_time = re.findall('(?<=Decode \(\d*\ tasks\):\s+)[\d\.]+', lines)
+    decode_time_list = list(map(float, decode_time))
 
+    total_time = re.findall('(?<=Total:\s+)[\d\.]+', lines)
+    total_time_list = list(map(float, total_time))
 
-BW = re.findall('(?<=Beamweights \(\d*\ tasks\):\s+)[\d\.]+',lines);
-BW_time = list(map(float, BW))
+    total_time_list = total_time_list[thres:len(total_time_list)-thres]
 
+    return total_time_list, fft_time_list, csi_time_list, bw_time_list, demul_time_list, decode_time_list
 
-Demul = re.findall('(?<=Demul \(\d*\ tasks\):\s+)[\d\.]+',lines);
-Demul_time = list(map(float, Demul))
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("--file", type="string", dest="file_name", help="file name as input", default="")
+    (options, args) = parser.parse_args()
+    file_str = options.file_name
 
-
-Decode = re.findall('(?<=Decode \(\d*\ tasks\):\s+)[\d\.]+',lines);
-Decode_time = list(map(float, Decode))
-
-
-a = re.findall('(?<=Total:\s+)[\d\.]+',lines);
-total = list(map(float, a))
-
-total_after = total[thres:len(total)-thres]
-
-
-max_index = total_after.index(max(total_after))
-print("max total time is: ", total_after[max_index])
-print("max total time after is: ", total[max_index+thres])
-print("\n")
-print("max fft time is: ", fft_time[max_index+thres])
-print("max csi time is: ", csi_time[max_index+thres])
-print("max BW time is: ", BW_time[max_index+thres])
-print("max demul time is: ", Demul_time[max_index+thres])
-print("max decode time is: ", Decode_time[max_index+thres])
-print("\n")
-print("Double check, sum of the previous ", fft_time[max_index+thres]+csi_time[max_index+thres]+BW_time[max_index+thres]+
-     Demul_time[max_index+thres]+Decode_time[max_index+thres])
-
-
-# print(len(total))
-# print(len(fft_time))
-# print(len(BW_time))
-# print(len(csi_time))
-# print(len(Demul_time))
-# print(len(Decode_time))
+    total_time, fft_time, csi_time, bw_time, demul_time, decode_time = proc_time(filename=file_str, thres=THRES)
+    
+    thres = THRES
+    max_index = total_time.index(max(total_time))
+    # sum = fft_time[max_index+thres] + csi_time[max_index+thres]+\
+    #       bw_time[max_index+thres]+demul_time[max_index+thres]+\
+    #       decode_time[max_index+thres]
+    print("Num of frames in the log = {}".format(len(total_time) + 2*thres))
+    print("Discard leading {} frames and trailing {} frames".format(thres, thres))
+    print("=> {} frames analyzed".format(len(total_time)))
+    print("Max frame processing time = {:.2f} ms, which has".format(total_time[max_index]))
+    print(". FFT time    = {:.2f} ms".format(fft_time[max_index+thres]))
+    print(". CSI time    = {:.2f} ms".format(csi_time[max_index+thres]))
+    print(". BW time     = {:.2f} ms".format(bw_time[max_index+thres]))
+    print(". Demod. time = {:.2f} ms".format(demul_time[max_index+thres]))
+    print(". Decode time = {:.2f} ms".format(decode_time[max_index+thres]))
+    # print("Sum of the previous = {:.2f} ms (double check)".format(sum))
+    # print(len(total_time))
+    # print(len(fft_time))
+    # print(len(bw_time))
+    # print(len(csi_time))
+    # print(len(demul_time))
+    # print(len(decode_time))
