4fdd9fd2f0f0163183c6137d101d0b229654dcb2
cstandy <cst.andy@gmail.com>
2024-11-11 19:44:25 -0500

    Feat: Add Agora's default setup in data rate estimator

--- estimator/estimate_data_rate.py
+++ estimator/estimate_data_rate.py
@@ -27,6 +27,21 @@ if __name__ == '__main__':
     ############################
     # Setup/calculate parameters
     ############################
+    # # Agora standard setup (except for the antenna count, which is a variable)
+    # numerology = 0
+    # scs = 15e3 * 2 ** numerology # Hz, subcarrier spacing
+    # antenna_bs = 8
+    # fft_size = 2048
+    # bit_iq = 16 # 32 bits per I/Q sample (16-b I/16-b Q)
+    # sampling_rate = scs * fft_size # Hz
+    # num_prb = 100
+    # num_data_sc = 12 * num_prb
+    # bandwidth = num_data_sc * scs
+    # modulation = '64QAM'
+    # mod_order = get_mod_order(modulation)
+    # code_rate = 0.333
+
+    # Savannah standard setup
     numerology = 3
     scs = 15e3 * 2 ** numerology # Hz, subcarrier spacing
     antenna_bs = 2
@@ -35,6 +50,7 @@ if __name__ == '__main__':
     sampling_rate = scs * fft_size # Hz
     num_prb = 66
     num_data_sc = 12 * num_prb
+    bandwidth = num_data_sc * scs
     modulation = '64QAM'
     mod_order = get_mod_order(modulation)
     code_rate = 438/1024
@@ -49,6 +65,7 @@ if __name__ == '__main__':
                ['Sampling rate (MHz)', sampling_rate/10**6],
                ['Number of physical resource blocks (PRBs)', num_prb],
                ['Number of data subcarriers', num_data_sc],
+               ['Bandwidth (MHz)', bandwidth/10**6],
                ['Modulation', modulation],
                ['Modulation order', mod_order],
                ['Code rate', '{}/1024'.format(code_rate*1024)]]
